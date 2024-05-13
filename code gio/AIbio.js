bb4d3bd2c607cfcaad42971d58c6761d0e8716f2
razanAlazzani <razanalazzani14@gmail.com>
2024-05-13 23:09:14 +0300

    Add files via upload

---
+++ code gio/AIbio.js
@@ -0,0 +1,29 @@
+// const api = "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";  
+// const inp = document.getElementById('inp')  
+// const images = document.querySelector('.images')  
+  
+// const getImage = async () => {  
+//     // make a request to openai api  
+//     const methods = {  
+//         methods:"POST",  
+//         headers:{  
+//             "Content-Type":"application/json",  
+//             "Authorization":`Bearer ${api}`  
+  
+//         },  
+//         body:JSON.stringify(  
+//             {  
+//                 "prompt":inp.value,  
+//                 "n":3,  
+//                 "size":"256x256"  
+//             }  
+//         )  
+//     }  
+//         const res = await fetch("https://api.openai.com/v1/images/generations", methods)
+//     // parse the response as json  
+//     const data = await res.json()  
+    
+//     console.log(data)
+    
+// }
+/************************************************************************************************ */
